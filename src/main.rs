f444f82ef121b2ab432c290785b12a5cd397162f
paulsullivanjr <paulsullivan1@gmail.com>
2024-01-05 07:33:48 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+async fn hello_world(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, World!".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening on http://{}", addr);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
